5b6ec3858b867736fc40a3da3f024ec638775f13
degalih <galihmramdan17@gmail.com>
2021-12-23 17:53:50 +0700

    kirim data baru

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
 const app = Vue.createApp({
-    data(){
-        return{
-            showBooks: true,
-            title: "Sebuah Seni Bersikap Bodo Amat",
-            author: "Mark Manson",
-            publisher: "Gramedia Indonesia",
-            year: 2016
-        }
+  data() {
+    return {
+      showBooks: true,
+      title: 'Sebuah Seni Bersikap Bodo Amat',
+      author: 'Mark Manson',
+      publisher: 'Gramedia Indonesia',
+      year: 2016,
+    };
+  },
+  methods: {
+    changeTitle(title) {
+      this.title = title;
     },
-    methods:{
-        changeTitle(title){
-            this.title= title
-        },
-        toogleShowBooks(){
-            this.showBooks= !this.showBooks;
-        }
-    }
-})
+    toogleShowBooks() {
+      this.showBooks = !this.showBooks;
+    },
+  },
+});
 
 app.mount('#app');
